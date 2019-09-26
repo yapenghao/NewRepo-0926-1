da4d9eaa52efe5ff1ca05f9283a715e1c05c668d
yapeng hao <yapeng.hao@email.com>
2019-09-26 09:54:43 -0400

    first master

---
+++ ConsoleApp0926-1/ConsoleApp0926-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0926_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Waster");
+        }
+    }
+}
